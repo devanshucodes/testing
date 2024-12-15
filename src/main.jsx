fdafd93ce904695a2f814823612ac2e7bec9b863
Devanshu Saxena <86067292+devanshu0605@users.noreply.github.com>
2024-12-15 18:49:25 +0530

    new

--- src/main.jsx
+++ src/main.jsx
@@ -9,9 +9,11 @@ import React from 'react';
     );
 
     function App() {
+      const initDataRaw = useLaunchParams()?.initDataRaw;
       return (
         <div className="container">
           <h1>Hello, world!</h1>
+          <div>{initDataRaw}</div>
         </div>
       );
     }
